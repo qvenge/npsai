b92236de91c41d947a28008dc9a81846e79f4782
qvenge <coovenbm@gmail.com>
2025-06-09 04:32:20 +0300

    Fix global error

---
+++ src/app/global-error.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import styles from './error.module.scss';
+import { Button } from '@/shared/ui';
+
+export default async function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+    <html>
+      <body className={styles.root}>
+        <h2 className={styles.message}>Что-то пошло не так</h2>
+        <div className={styles.reloadButton}>
+          <Button
+            type="primary"
+            size="l"
+            onClick={() => reset()}
+          >
+            Перезагрузить
+          </Button>
+        </div>
+      </body>
+    </html>
+  );
+}
